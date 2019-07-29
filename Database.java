7ac70810e34ac92321b5d38eb12961a6578801ab
sidhantsing <53421139+sidhantsing@users.noreply.github.com>
2019-07-29 11:42:07 +0530

    Add files via upload
    The database and the datauploader java file will fetch the data from the csv file and populate it in the database created using sql.

---
+++ Database.java
@@ -0,0 +1,142 @@
+import java.io.*;
+import java.sql.*;
+public class Database {
+	int empid;
+	String age;
+	String gender;
+	String family_history;
+	String treatment;
+	String work_interfere;
+	String remote_work;
+	String benfits;
+	String wellness_program;
+	String seek_help;
+	String anonymity;
+	String mental_health_consequence;
+	String physical_health_consequence;
+	String coworkers;
+	String supervisor;
+	String mental_health_interview;
+	String physical_health_interview;
+	String mental_vs_physical;
+	String predict_leave;
+	
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getFamily_history() {
+		return family_history;
+	}
+	public void setFamily_history(String family_history) {
+		this.family_history = family_history;
+	}
+	public String getTreatment() {
+		return treatment;
+	}
+	public void setTreatment(String treatment) {
+		this.treatment = treatment;
+	}
+	public String getWork_interfere() {
+		return work_interfere;
+	}
+	public void setWork_interfere(String work_interfere) {
+		this.work_interfere = work_interfere;
+	}
+	public String getRemote_work() {
+		return remote_work;
+	}
+	public void setRemote_work(String remote_work) {
+		this.remote_work = remote_work;
+	}
+	public String getBenfits() {
+		return benfits;
+	}
+	public void setBenfits(String benfits) {
+		this.benfits = benfits;
+	}
+	public String getWellness_program() {
+		return wellness_program;
+	}
+	public void setWellness_program(String wellness_program) {
+		this.wellness_program = wellness_program;
+	}
+	public String getSeek_help() {
+		return seek_help;
+	}
+	public void setSeek_help(String seek_help) {
+		this.seek_help = seek_help;
+	}
+	public String getAnonymity() {
+		return anonymity;
+	}
+	public void setAnonymity(String anonymity) {
+		this.anonymity = anonymity;
+	}
+	public String getMental_health_consequence() {
+		return mental_health_consequence;
+	}
+	public void setMental_health_consequence(String mental_health_consequence) {
+		this.mental_health_consequence = mental_health_consequence;
+	}
+	public String getPhysical_health_consequence() {
+		return physical_health_consequence;
+	}
+	public void setPhysical_health_consequence(String physical_health_consequence) {
+		this.physical_health_consequence = physical_health_consequence;
+	}
+	public String getCoworkers() {
+		return coworkers;
+	}
+	public void setCoworkers(String coworkers) {
+		this.coworkers = coworkers;
+	}
+	public String getSupervisor() {
+		return supervisor;
+	}
+	public void setSupervisor(String supervisor) {
+		this.supervisor = supervisor;
+	}
+	public String getMental_health_interview() {
+		return mental_health_interview;
+	}
+	public void setMental_health_interview(String mental_health_interview) {
+		this.mental_health_interview = mental_health_interview;
+	}
+	public String getPhysical_health_interview() {
+		return physical_health_interview;
+	}
+	public void setPhysical_health_interview(String physical_health_interview) {
+		this.physical_health_interview = physical_health_interview;
+	}
+	public String getMental_vs_physical() {
+		return mental_vs_physical;
+	}
+	public void setMental_vs_physical(String mental_vs_physical) {
+		this.mental_vs_physical = mental_vs_physical;
+	}
+	public String getPredict_leave() {
+		
+		return predict_leave;
+	}
+	public void setPredict_leave(String predict_leave) {
+		this.predict_leave = predict_leave;
+	}
+	
+	
+}
